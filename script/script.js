466eb1e9b785b14ebaf8984019ea18b94be3bfd9
Nayimh <hnaim6979@gmail.com>
2021-08-21 18:29:10 +0600

    second commit

--- script/script.js
+++ script/script.js
@@ -5,6 +5,26 @@ const grandTotal = document.getElementById('finalTotal');
 
 
 // global veriable
+
+// no ideo what to do
+function addPromoCode(){
+    const promoInputText = document.getElementById('promoText');
+    const promoInput = promoInputText.value;
+
+    const discount = (getTotal() * 20) / 100;
+
+    if(promoInput.toLowerCase() == 'stevekaku'){
+        grandTotal.innerText = getTotal() - discount;
+        promoInputText.value = '';
+        document.getElementById('matched').innerText = 'You got 20% Cash Back!';
+        document.getElementById('matched').value = '';
+    }else{
+        document.getElementById('error').innerText = 'Invalid Promo code!';
+        promoInputText.value = '';
+        document.getElementById('erron').value = '';
+       
+    }
+}
 // function
 function getServiceNumber(value, input) {
     document.getElementById(value).innerText = input;
@@ -13,36 +33,20 @@ function getServiceNumber(value, input) {
 
 function getTotal() {
 const bestPriceNumber = parseInt(bestPrice.innerText);
-const momoeryCost = parseInt(memoryCost.innerText);
+const momoryCost = parseInt(memoryCost.innerText);
 const storageCost = parseInt(StorageCost.innerText);
     const shippingCost = parseInt(deliveryCost.innerText);
-    const totalPrice = bestPriceNumber + momoeryCost + storageCost + shippingCost;
+    const totalPrice = bestPriceNumber + momoryCost + storageCost + shippingCost;
     finalPrice.innerText = totalPrice;
 
-    const grandTotalprice = totalPrice;
-    grandTotal.innerText = grandTotalprice;
-    return grandTotalprice;
+    const grandTotalPrice = totalPrice;
+    grandTotal.innerText = grandTotalPrice;
+    return grandTotalPrice;
 
 
 }
 
-// no ideo what to do
-// function addPromoCode(){
-//     const promoInput = document.getElementById('promoText');
-//     const promoInput = promoInput.value;
 
-//     const discount = (getTotal() * 20) / 100;
-
-//     if(promoInput.toLowerCase() == 'stevekaku'){
-//         finalTotal.innerText = getTotal() - discount;
-//         promoInput.value = '';
-//         // document.getElementById('matched').innerText = 'You got 20% Cash Back!';
-//     }else{
-//         // document.getElementById('error').innerText = 'Invalid Promo code!';
-//         promoInput.value = '';
-//         document.getElementById('matched').innerText = '';
-//     }
-// }
 
 
 // event 
